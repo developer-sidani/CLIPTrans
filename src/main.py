14a93012154f0389225e620c517a7a7170728484
developer-sidani <developer.sidani@gmail.com>
2024-11-30 00:36:21 +0100

    fix: comet name

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ def main(params):
     comet_api_key = os.getenv('COMET_API_KEY')
     comet_project = os.getenv('COMET_PROJECT_NAME')
     comet_workspace = os.getenv('COMET_WORKSPACE')
-    experiment = Experiment(api_key=comet_api_key, project_name=comet_project, workspace=comet_workspace, experiment_config=ExperimentConfig(name=f"{params.mn}_{params.stage}_{params.tgt_lang}", tags=[params.stage, params.tgt_lang]))
+    experiment = Experiment(api_key=comet_api_key, project_name=comet_project, workspace=comet_workspace, experiment_config=ExperimentConfig(name=f"{params.model_name}_{params.stage}_{params.tgt_lang}", tags=[params.stage, params.tgt_lang]))
     experiment.log_parameters(vars(params))  # Log all parameters
     
     if params.num_gpus > 1:
